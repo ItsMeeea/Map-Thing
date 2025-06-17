da10a506d8567afabb50bfa46db336aaf66bc752
Kailu Baradas <jankailubaradas@gmail.com>
2025-06-17 18:24:05 +0800

    added Blind Search Algo
    added Blind Search Algo

--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+from collections import OrderedDict, deque
 
 letter_names = OrderedDict([
     ("A", "Andrew - Br. Andrew Gonzales Hall"),
@@ -40,93 +40,113 @@ letter_names = OrderedDict([
     ("BD", "Fidel A. Reyes St.")
 ])
 
-# regarding the adjacency list below:
-
-# map / graph thing and also the table below is in the google docs
-
-# first row is the A* score of each col (used for A* search)
-# ex: B has A* score of 4.5
-
-# for the rest, rows (A-M, BA-BD) are the start, cols (A-M, AA-AT, BA-BD) are the goals
-# ex: look at line 63 if you wanna start at point A, line 69 if point G, line 75 if point M
-# ex: from point G to point B, it's 75 meters away
-
-# A-M represent dlsu bldgs above in order
-# AA-AT represent food list above in order
-# BA, BB, BC, BD arent on campus (e.g. Agno Street); they're just used in the algo
-
 adjacency_list = [
     #  A    B    C    D    E    F    G    H    I    J    K    L    M    AA   AB   AC   AD   AE   AF   AG   AH   AI   AJ   AK   AL   AM   AN   AO   AP   AQ   AR   AS   AT   BA   BB   BC   BD
     [ 5.0, 4.5, 3.0, 3.0, 3.0, 4.8, 3.0, 5.0, 3.0, 4.0, 3.0, 3.0, 3.0, 4.7, 3.9, 4.0, 4.0, 3.0, 4.0, 5.0, 4.0, 3.0, 4.9, 4.8, 4.2, 4.0, 4.0, 3.0, 4.2, 3.0, 4.0, 3.0, 3.9, 3.0, 3.0, 3.0, 3.0],
-    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,  49],
-    [   0,   0,   0,   0,   0,   0,  75,   0,  83,   0,   0,  70,   0,   0,   0,  0,   0,   0,   1,   1,   0,   0,   1,   1,   0,   0,   0,    0,   0,   1,   0,   0,   0,   0,   0,   0,   0],
+    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,  49],
+    [   0,   0,   0,   0,   0,   0,  75,   0,  83,   0,   0,  70,   0,   0,   0,   0,   0,   0,   1,   1,   0,   0,   1,   1,   0,   0,   0,    0,   0,   1,   0,   0,   0,   0,   0,   0,   0],
     [   0,   0,   0,   0,   0,   0,   0, 280,   0,   0,   0,   0,  52,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
-    [   0,   0,   0,   0,   0,   0, 140,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
-    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,  36,   0,   0],
+    [   0,   0,   0,   0,   0,   0, 140,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
+    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,  36,   0,   0,   0,   0],
     [   0,   0,   0,   0,   0,   0, 180, 190,   0,   0,   0, 200, 110,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
     [   0,  75,   0, 140,   0, 180,   0,   0,  97,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
-    [   0,   0, 280,   0,   0, 190,   0,   0,   0,   0,   0,   0, 400,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0, 110,   0,   0,   0,   0,   0,   0,   0,   0],
+    [   0,   0, 280,   0,   0, 190,   0,   0,   0,   0,   0,   0, 400,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0, 110,   0,   0,   0,   0,   0,   0,   0,   0,   0],
     [   0,  83,   0,   0,   0,   0,  97,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,  51,   0,   0],
     [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   1,   0,   0,   0,   0,   0,  39],
     [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0, 140],
     [   0,  70,   0,   0,   0, 200,   0,   0,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,  61,   0,   0],
     [   0,   0,  52,   0,   0, 110,   0, 400,   0,   0,   0,   0,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
-    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,  0,   0,   0,   0,   0,   1,   1,   0,   0,   0,   1,   1,    0,   0,   0,   0,   1,   0,   0,   1,   0,   0],
+    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   1,   1,   0,   0,   0,   1,   1,    0,   0,   0,   0,   1,   0,   0,   1,   0,   0],
     [   0,   0,   0,   0,   1,   0,   0,   0,   1,   0,   0,   1,   0,   0,   0,  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   1,   0,   1,   1],
-    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   0,  1,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,    1,   0,   0,   0,   0,   1,   0,   1,   0,   0]
-    [   1,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   0,   0,   0,   0,  0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   1,   0,   0]
+    [   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   0,   1,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,    1,   0,   0,   0,   0,   1,   0,   1,   0,   0],
+    [   1,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   0,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,   0,   1,   0,   0]
 ]
+
+def build_graph():
+    """
+    Build a bidirectional adjacency list (graph) from the adjacency matrix.
+    """
+    graph = {}
+    keys_list = list(letter_names.keys())
+    n = len(keys_list)
+
+    for i in range(2, len(adjacency_list)):
+        src = keys_list[i - 2]
+        graph.setdefault(src, [])
+        for j, weight in enumerate(adjacency_list[i][:n]):
+            if weight > 0:
+                dst = keys_list[j]
+                graph.setdefault(dst, [])
+                graph[src].append(dst)
+                graph[dst].append(src)
+    return graph
 
 def blind_search(start, end):
     """
-    Performs a blind search to find the best path between the start and end points
-    Prints the list of places to pass through
-    Also the total distance, total cost, time it took to calculate, other stats
-
-    Args:
-        start (str): start point letter
-        end (str): end point letter
+    Perform a simple BFS (blind) search from start to end.
     """
-    print("\u001b[33mhello blind")
+    graph = build_graph()
+    visited = set()
+    queue = deque([[start]])
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+
+        if node == end:
+            print("Path:", " → ".join(path))
+            print("Steps:", len(path) - 1)
+            return
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph.get(node, []):
+                queue.append(path + [neighbor])
+
+    print("No path found.")
 
 def heuristic_search(start, end):
     """
-    Performs a heuristic search to find the best path between the start and end points
-    Prints the list of places to pass through
-    Also the total distance, total cost, time it took to calculate, other stats
-
-    Args:
-        start (str): start point letter
-        end (str): end point letter
+    Placeholder for heuristic search (e.g., A*).
     """
-    print("\u001b[35mhello heuristic")
+    print("\u001b[35mhello heuristic\u001b[0m")
 
 if __name__ == "__main__":
-    dlsu_number = 13 # no of bldgs
-    food_number = 20 # no of foods
+    dlsu_number = 13  # campus buildings
+    food_number = 20  # food stalls
     letters_list = list(letter_names.keys())
-    places_list = list(letter_names.values())
+    places_list  = list(letter_names.values())
 
+    # Select campus building
     while True:
         print("\u001b[42m\nWhere are you?\u001b[0m\u001b[32m")
         for i in range(dlsu_number):
-            print("[" + str(i+1) + "] " + places_list[i], sep="")
-        option = int(input("\n\u001b[0mYour option: "))
-        if(option < 1  or option > 14):
-            print("\u001b[31mInvalid Input!")
+            print(f"[{i+1}] {places_list[i]}")
+        try:
+            option = int(input("\n\u001b[0mYour option: "))
+        except ValueError:
+            print("\u001b[31mInvalid input! Please enter a number.\u001b[0m")
+            continue
+        if option < 1 or option > dlsu_number:
+            print("\u001b[31mInvalid Input!\u001b[0m")
         else:
             start = letters_list[option - 1]
             break
 
+    # Select food stall
     while True:
         print("\u001b[44m\nWhere do you want to eat?\u001b[0m\u001b[34m")
         for i in range(food_number):
-            print("[" + str(i+1) + "] " + places_list[dlsu_number + i], sep="")
-        option = int(input("\n\u001b[0mYour option: "))
-        if(option < 1  or option > 20):
-            print("\u001b[31mInvalid Input!")
+            print(f"[{i+1}] {places_list[dlsu_number + i]}")
+        try:
+            option = int(input("\n\u001b[0mYour option: "))
+        except ValueError:
+            print("\u001b[31mInvalid input! Please enter a number.\u001b[0m")
+            continue
+        if option < 1 or option > food_number:
+            print("\u001b[31mInvalid Input!\u001b[0m")
         else:
-            end = letters_list[option - 1 + dlsu_number]
+            end = letters_list[dlsu_number + option - 1]
             break
 
     print("\u001b[43m\nYour shortest route using BLIND search:\u001b[0m")
@@ -135,4 +155,4 @@ if __name__ == "__main__":
     print("\u001b[45m\nYour shortest route using HEURISTIC search:\u001b[0m")
     heuristic_search(start, end)
 
-    print("\u001b[0m") # reset colors
+    print("\u001b[0m")  # reset colors
